65b674841e82b07941dbde1465f3277331aa92db
megisekhnishvili <megi.sekhniashvili.1@btu.edu.ge>
2024-06-19 00:00:30 +0400

    10th commit

---
+++ Davaleba10/portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="your-image-url.jpg" className="profile-pic" alt="profile" />
+        <h1>Welcome to My Portfolio</h1>
+        <p>
+          Hi, I'm [Your Name], a [Your Profession]. I love to work on exciting projects and solve interesting problems.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
